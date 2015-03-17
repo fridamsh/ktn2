58561ea3c3e5d913f841693537c4e387db3abc81
Frida <sch_frida@hotmail.com>
2015-03-17 11:42:36 +0100

    i also got inspired.. and wrote code

---
+++ Client.py
@@ -0,0 +1,69 @@
+import socket
+import json
+from MessageWorker import MessageReceiver
+
+class Client(object):
+    """
+    This is the chat client class
+    """
+
+    def __init__(self):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, server_port):
+        self.connection.connect((host, server_port))
+
+        serverThread = MessageReceiver(client, self.connection)
+        serverThread.daemon = True
+        serverThread.start()
+
+        print "Welcome to AwzmChat<3 write something awezome - aand be awezome."
+        print "Received thread: ", serverThread.name
+
+    def receive_message(self, message, connection):
+        response = json.loads(message)
+
+        if response.get('error') is not None:
+            print response.get('error')
+        elif response.get('response') == 'login':
+            print "Welcome, " + response.get('username') + ", to AwzmChat<3"
+            for message in response.get('messages'):
+                print message
+        elif response.get('response') == 'logout':
+            print "Byebye " + response.get('username')
+        elif response.get('response') == 'message':
+            print response.get('message')
+
+    def connection_closed(self, connection):
+        connection.close()
+
+    def send(self, data):
+        if data.startswith("*login"):
+            try:
+                username = data.split()[1]
+            except IndexError:
+                username = ""
+            data = {'request': 'login', 'username': username}
+        elif data.startswith("*logout"):
+            data = {'request': 'logout'}
+        else:
+            data = {'request': 'message', 'message': data}
+
+        self.connection.sendall(json.dumps(data))
+
+    def force_disconnect(self):
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.start('localhost', 9999)
+
+    while True:
+        message = raw_input('-- ')
+        client.send(message)
+        
+        if message == '*exit':
+            break
+
+    client.force_disconnect()
---
+++ Server.py
@@ -0,0 +1,119 @@
+'''
+KTN-project 2013 / 2014
+Very simple server implementation that should serve as a basis
+for implementing the chat server
+'''
+import SocketServer
+import json
+import time
+import datetime
+import re
+
+'''
+The RequestHandler class for our server.
+It is instantiated once per connection to the server, and must
+override the handle() method to implement communication to the
+client.
+'''
+
+
+class ClientHandler(SocketServer.BaseRequestHandler):
+
+    def printPretty(self, message, username, timestamp):
+        return timestamp + " " + username + ' | ' + message
+
+    def timestamp(self):
+        return str(datetime.now().strftime('%Y/%m/%d %H:%M'))
+
+    def login(self, json_object):
+        username = json_object.get('username')
+        pattern = '^\w+$'
+        if not re.match(pattern, username):
+            return_data = {'response': 'login', 'error': 'Invalid username!', 'username': username}
+            self.connection.sendall(json.dumps(return_data))
+        elif username in self.server.clients.values():
+            return_data = {'response': 'login', 'error': 'Name already taken!', 'username': username}
+            self.connection.sendall(json.dumps(return_data))
+        else:
+            self.server.clients[self.connection] = username
+            return_data = {'response': 'login', 'username': username, 'messages': self.server.messages}
+            self.connection.sendall(json.dumps(return_data))
+
+    def logout(self):
+
+        if not self.connection in self.server.clients:
+            return_data = {'response': 'logout', 'error': 'Not logged in!'}
+            self.connection.sendall(json.dumps(return_data))
+        else:
+            username = self.server.clients[self.connection]
+            return_data = {'response': 'logout', 'username': username}
+            self.connection.sendall(json.dumps(return_data))
+            del self.server.clients[self.connection]
+
+    def send_message(self, json_object):
+        if not self.connection in self.server.clients:
+            return_data = {'response': 'message', 'error': 'Not logged in!'}
+            self.connection.sendall(json.dumps(return_data))
+        else:
+            username = self.server.clients[self.connection]
+            json_message = json_object.get('message')
+            ts = time.time()
+            st = datetime.datetime.fromtimestamp(ts).strftime('%d.%m.%Y %H:%M')
+            message = self.printPretty(json_message, username, st)
+            self.server.messages.append(message)
+            return_data = {'response': 'message', 'message': message}
+            self.server.broadcast(json.dumps(return_data))
+
+    def handle(self):
+        # Get a reference to the socket object
+        self.connection = self.request
+        # Get the remote ip adress of the socket
+        self.ip = self.client_address[0]
+        # Get the remote port number of the socket
+        self.port = self.client_address[1]
+
+        print 'Client connected @' + self.ip + ':' + str(self.port)
+        # Wait for data from the client
+        # Check if the data exists
+        # (recv could have returned due to a disconnect)
+
+        while True:
+            data = self.connection.recv(1024).strip()
+            if data:
+                json_object = json.loads(data)
+                request = json_object.get('request')
+                if request == 'login':
+                    self.login(json_object)
+                elif request == 'logout':
+                    self.logout()
+                elif request == 'message':
+                    self.send_message(json_object)
+            else:
+                break
+
+        print 'Client disconnected @' + self.ip + ':' + str(self.port)
+    def finish(self):
+        if self.connection in self.server.clients:
+            del self.server.clients[self.connection]
+
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    allow_reuse_address = True
+    messages = []
+    clients = {}
+
+    def broadcast(self, message):
+        for client in self.clients:
+            client.sendall(message)
+
+
+if __name__ == "__main__":
+    HOST = 'localhost'
+    PORT = 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = ThreadedTCPServer((HOST, PORT), ClientHandler)
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
